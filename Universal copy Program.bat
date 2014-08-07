57f3d635b558b1e937d411ea6b8375c595df375a
Ugion <samuel@tfwno.gf>
2014-08-07 18:48:34 +0200

    Fixed grammar, removed unnecessary code.

--- Universal copy Program.bat
+++ Universal copy Program.bat
@@ -1,26 +1,26 @@
 @echo off
 :home
-echo What do you want to copy?
+echo XtraUniCopy
+echo What do you want to do?
 echo ========================
-echo.
 echo 1. Copy smothing
-echo 2. goto Google
+echo 2. Go to Google
 echo 3. Exit this shit
+echo ========================
 set /p web=Type option:
-if "%web%"=="1" goto copy
+if "%web%"=="1"goto copy
 if "%web%"=="2" goto google
 if "%web%"=="3" exit
 :copy
-set /p Copy1=Type the copy destination:
-set /p Copy2=Type the "copy to" destiation
+set /p Copy1=File you want to copy:
+set /p Copy2=Folder you want to copy to:
 copy %Copy1% %Copy2%
 cls
 echo Done!
 cls
 goto home
 :google
-run www.google.com
+START http://www.google.com
 cls
 goto home
-:exit
 exit 
